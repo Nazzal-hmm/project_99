929f3b7adbffadafb946117c798ca0cdb8bf399a
Abdul <desissaa@gmail.com>
2025-03-19 00:08:23 +0000

    task-progress

--- abdulbranch/script/script.js
+++ abdulbranch/script/script.js
@@ -1,7 +1,6 @@
 //Create Account    
 document.addEventListener("DOMContentLoaded", function () {
     sessionStorage.clear();
-
     document.querySelector(".container").addEventListener("submit", function (event) {
         event.preventDefault();
 
@@ -28,4 +27,39 @@ container.style.display = "block"
 function closeIframe(){
 document.getElementById("settingsContainer").style.display = "none";
 }
+
+//Scenes
+document.addEventListener("DOMContentLoaded", () => {
+    let userInput = document.getElementById("userIn");
+    let displayBlock =document.getElementById("displayBlock")
+    
+    userInput.addEventListener("keydown", (e) => {
+
+        //Scene1
+        if (window.location.pathname === "/sites/scene1.html" && e.key === "Enter") {
+            let inputValue = userInput.value.trim();
+
+            if (inputValue === "go ship") {
+                location.href = "/sites/task4final.html";
+            } else {
+               displayBlock.innerHTML= inputValue+" is an Invalid input!<br>Type 'go ship' to change location!";
+               userInput.value="";
+            }
+        }
+
+        //Task4final
+        if (window.location.pathname === "/sites/task4final.html" && e.key === "Enter") {
+            let inputValue = userInput.value.trim();
+     
+            if(inputValue === "go back"){
+                location.href = "/sites/scene1.html";
+             } else{
+                 displayBlock.innerHTML= inputValue+" is an Invalid input!<br>Type 'go back' to change location!";
+                 userInput.value="";
+             }
+       }
+        
+    });
+});
+
 
