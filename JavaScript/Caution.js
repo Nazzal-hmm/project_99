c63abe50f898a97bb58a0220dfd282029421fdd2
Christy Chovalloor <christy.chovalloor@gmail.com>
2025-03-21 02:45:24 +0000

    First HTML, CSS and JS for Task 2

---
+++ JavaScript/Caution.js
@@ -0,0 +1,28 @@
+// Open Settings Button
+function openSettings() {
+const container = document.getElementById("settingsContainer");
+container.style.display = "block"
+}
+function closeIframe(){
+document.getElementById("settingsContainer").style.display = "none";
+}
+
+
+
+document.addEventListener("DOMContentLoaded",() =>{
+  const userInput = document.querySelector(".terminal");
+  const message = document.getElementById("message");
+
+  userInput.addEventListener("keydown",(e)=> {
+    if (e.key === "Enter"){
+      let inputValue = userInput.value.trim().toLowerCase();
+
+      if (inputValue === "what happened"){
+        window.location.href = "../HTML/task2intro.html";
+      }else{
+        message.innerHTML = `   "${inputValue}" is incorrect! Try again.`;
+        message.style.color = "red";
+      }
+    }
+  })
+})
